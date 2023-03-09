823dfed9d9238a611d7a97a64c352e4f3c8702d6
ericasaline <ericasaline@gmail.com>
2023-03-09 13:49:32 -0300

    Customizacao

--- App/app/src/main/java/com/app/contactlist/MainActivity.kt
+++ App/app/src/main/java/com/app/contactlist/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.app.contactlist.ui.theme.ContactListTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +19,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ContactListTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -31,7 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        color = Color.Magenta,
+        style = MaterialTheme.typography.h1,
+        fontSize = 28.sp
+    )
 }
 
 @Preview(showBackground = true)
